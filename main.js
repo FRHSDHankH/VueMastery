786e54bc5f5f20c5a59922d81d5305439d9d7109
FRHSDHankH <526hhabashy@frhsd.com>
2025-11-25 13:11:14 -0500

    Event Handling

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 const app = Vue.createApp({
   data() {
     return {
-      product: 'Socks',
-      description: 'Keeps feet warm',
-      image: './assets/images/socks_green.jpg',
+      product: "Socks",
+      description: "Keeps feet warm",
+      image: "./assets/images/socks_green.jpg",
       inventory: 15,
       onSale: false,
-      details: ['50% cotton', '30% wool', '20% polyester'],
-      sizes: ['S', 'M', 'L', 'XL'],
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      sizes: ["S", "M", "L", "XL"],
       variants: [
-        { id: 2234, color: 'green' },
-        { id: 2235, color: 'blue' },
-      ]
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
     };
-  }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart >= 1) {
+        this.cart -= 1;
+      }
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+  },
 });
