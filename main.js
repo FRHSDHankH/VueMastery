32493ee4a50d648efdfbcb43b6cfd80e2a26071c
FRHSDHankH <526hhabashy@frhsd.com>
2025-11-25 13:04:31 -0500

    Attribute Binding

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      description: 'Keeps feet warm'
+      description: 'Keeps feet warm',
+      image: './assets/images/socks_green.jpg'
     };
   }
 });
